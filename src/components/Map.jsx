366c88ccc2d63e6732104e01a97e3bc74ac878dc
Jan <jan.urankar@gmail.com>
2022-12-19 18:52:36 +0100

    Radiii

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,53 +1,66 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { GoogleMap, LoadScript, Polyline  } from '@react-google-maps/api';
 
-const containerStyle = {
-    marginLeft: '15%',
-    width: '70%',
-    height: '600px'
+const Map = () => {
+    const [path, setPath] = useState('no_data')
 
-};
+    // API CALL
+    const fetchApiData = async () => {
+        const res = await fetch("http://localhost:8002/api/maps")
+        const data = await res.json()
 
-const center = {
-    lat: 0,
-    lng: -180
-};
+        return data
+    }
 
-const onLoad = polyline => {
-    console.log('polyline: ', polyline)
-};
+    useEffect(() => {
+        const getResponse = async () => {
+            const responseFromServer = await fetchApiData()
+            setPath(responseFromServer.Koordinate)
+            console.log(path)
+        }
 
-const path = [
-    {lat: 37.772, lng: -122.214},
-    {lat: 21.291, lng: -157.821},
-    {lat: -18.142, lng: 178.431},
-    {lat: -27.467, lng: 153.027}
-];
+        getResponse()
+    }, [])
 
-const options = {
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-    radius: 30000,
-    paths: [
-        {lat: 37.772, lng: -122.214},
-        {lat: 21.291, lng: -157.821},
-        {lat: -18.142, lng: 178.431},
-        {lat: -27.467, lng: 153.027}
-    ],
-    zIndex: 1
-};
 
-function Map() {
+    // PATH
+    const onLoad = polyline => {
+        console.log('polyline: ', polyline)
+    };
+
+
+    // STYLE
+    const containerStyle = {
+        marginLeft: '15%',
+        width: '70%',
+        height: '600px'
+    };
+
+    const center = {
+        lat: path[0].lat,
+        lng: path[0].lng
+    };
+
+    const options = {
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        clickable: false,
+        draggable: false,
+        editable: false,
+        visible: true,
+        radius: 30000,
+        paths: path,
+        zIndex: 1
+    };
+
+
+    // RETURN
     return (
         <LoadScript
-            googleMapsApiKey="AIzaSyBhDyguP-Dme8qNjA_RM5CtGO8QwJyNQmc"
+            googleMapsApiKey=""
         >
             <GoogleMap
                 mapContainerStyle={containerStyle}
